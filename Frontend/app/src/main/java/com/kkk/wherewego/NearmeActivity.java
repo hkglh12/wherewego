480a417d85b87e9f0153d1cf8a473860f1dc01e4
hkglh12@gmail.com <hkglh12@gmail.com>
2019-07-22 23:37:10 +0900

    WIP : Front base construction:

---
+++ Frontend/app/src/main/java/com/kkk/wherewego/NearmeActivity.java
@@ -0,0 +1,14 @@
+package com.kkk.wherewego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NearmeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearme);
+    }
+}
